735180318d223f0a42e72fd0544852c693216c95
Miridon Troshupa <troshmir@students.zhaw.ch>
2019-10-27 17:04:10 +0100

    ganache

--- migrations/2_deploy_contracts.js
+++
@@ -1,8 +0,0 @@
-// migration files put contracts on the blockchain
-// #_ tells truffle in which order the contracts will be migrated
-
-var Image = artifacts.require("./Image.sol");
-
-module.exports = function(deployer) {
-  deployer.deploy(Image);
-};
